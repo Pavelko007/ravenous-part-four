0b5e43cfbe7bf8d4cff5637a9a00822676855d47
Pavlo Kvas <kvaspasha@gmail.com>
2024-06-01 21:55:13 +0300

    Refactor Components For API Response

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,21 +4,6 @@ import styles from "./App.module.css";
 import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
 
-const business = {
-  imageSrc:
-    "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
-  name: "MarginOtto Pizzeria",
-  address: "1010 Paddington Way",
-  city: "Bordertown",
-  state: "NY",
-  zipCode: "10101",
-  category: "Italian",
-  rating: 4.5,
-  reviewCount: 90,
-};
-
-const businesses = [business, business, business, business, business, business];
-
 const App = () => {
   const searchYelp = (term, location, sortBy) => {
     console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
@@ -28,7 +13,7 @@ const App = () => {
     <div className={styles.App}>
       <h1>ravenous</h1>
       <SearchBar searchYelp={searchYelp} />
-      <BusinessList businesses={businesses} />
+      <BusinessList businesses={null} />
     </div>
   );
 };
