eaeef2c735af484c5621699a23538125e3a99a2a
Pavlo Kvas <kvaspasha@gmail.com>
2024-06-01 17:30:44 +0300

    Retrieve Yelp API v3 Credentials

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App/App";
 
+const apiKey = "S1aS-cS0ZPK2pxKXP5Exr2rCq_oFkiPnBK7cjpBcnyyMQLPm36KeLikNFhQQgJJDCvjEuF5oMr2H0fj6nDDbpbMbJ8w9rKqNBKY9-xHcz8Bk4Aws0GOrskN4Sy9bZnYx";
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
